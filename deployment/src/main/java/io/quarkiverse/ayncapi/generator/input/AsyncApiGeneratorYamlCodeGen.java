e4af30e010338b4f669458790ddc868c31f77e6e
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2022-11-24 18:51:44 +0100

    Generator first version

---
+++ deployment/src/main/java/io/quarkiverse/ayncapi/generator/input/AsyncApiGeneratorYamlCodeGen.java
@@ -0,0 +1,8 @@
+package io.quarkiverse.ayncapi.generator.input;
+
+public class AsyncApiGeneratorYamlCodeGen extends AsyncApiPathGeneratorCodeGen {
+
+    public AsyncApiGeneratorYamlCodeGen() {
+        super(AsyncApiGeneratorCodeGenBase.YAML);
+    }
+}
---
+++ deployment/src/main/java/io/quarkiverse/ayncapi/generator/input/AsyncApiGeneratorYmlCodeGen.java
@@ -0,0 +1,8 @@
+package io.quarkiverse.ayncapi.generator.input;
+
+public class AsyncApiGeneratorYmlCodeGen extends AsyncApiPathGeneratorCodeGen {
+
+    public AsyncApiGeneratorYmlCodeGen() {
+        super(AsyncApiGeneratorCodeGenBase.YML);
+    }
+}
