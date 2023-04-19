92a124ee157fb06d2ed5e949a1227d7a807e5577
radtriste <tradisso@redhat.com>
2023-04-19 09:16:06 +0200

    Quarkus 3 update

--- runtime/src/main/java/io/quarkiverse/asyncapi/config/AsyncAPIRegistryProducer.java
+++ runtime/src/main/java/io/quarkiverse/asyncapi/config/AsyncAPIRegistryProducer.java
@@ -1,9 +1,9 @@
 package io.quarkiverse.asyncapi.config;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Instance;
-import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class AsyncAPIRegistryProducer {
